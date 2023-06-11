e5378ea2a77464d37b5ed6652d40845762170ca6
AntWader <p.anton.art.fis@gmail.com>
2023-06-11 03:23:41 +0300

    deploy.sh fix

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
-sudo docker compose up -d
-sudo docker compose up -d
+#sudo crontab cronjob_deploy
+
+echo "root cron job is running..."
+
+# in case that container already running
+sudo docker compose -f /var/www/deploy/docker-compose.yml down
+
+
+#sudo mkdir -p  /var/www/
+
+sudo rm -rf /var/www/deploy
+
+sudo cp /home/ubuntu/agent/work/db5eb7314bcd7c73/ /var/www/deploy
+
+
+sudo docker compose -f /var/www/deploy/docker-compose.yml up -d
+
+sudo crontab -r
